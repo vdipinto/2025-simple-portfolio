12c9e753ae86450b56d5f0e7c5296253960b9078
Vito Dipinto <vitodipinto@outlook.com>
2025-07-08 17:09:45 +0100

    add prisma Accelerate

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,8 +1,12 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from '@prisma/client'
+import { withAccelerate } from '@prisma/extension-accelerate'
 
-const globalForPrisma = global as unknown as { prisma: PrismaClient };
+const globalForPrisma = globalThis as unknown as { prisma: PrismaClient }
 
-export const prisma =
-  globalForPrisma.prisma || new PrismaClient();
+const prismaClient = globalForPrisma.prisma || new PrismaClient().$extends(withAccelerate())
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+if (process.env.NODE_ENV !== 'production') {
+  globalForPrisma.prisma = prismaClient
+}
+
+export const prisma = prismaClient
