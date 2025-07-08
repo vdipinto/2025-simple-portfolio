e0cfd4b4a8eb5b7c6f75f48f14bfa594f048b848
Vito Dipinto <vitodipinto@outlook.com>
2025-07-08 17:57:30 +0100

    add prisma Accelerate

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from '@prisma/client/edge'
 import { withAccelerate } from '@prisma/extension-accelerate'
 
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient }
